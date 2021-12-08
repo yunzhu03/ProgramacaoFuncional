a125c6e4fd9072971505e95842c99888d7b396c9
Yun Zhu <a100820@alunos.uminho.pt>
2021-12-08 12:26:25 +0000

    colclusão da ficha5

--- ficha5.hs
+++ ficha5.hs
@@ -11,12 +11,14 @@ module PF_fichas where
     
 --3
 
+matriz1 :: [[Integer]]
 matriz1 = [[2,4,0,6],
            [0,3,3,1],
            [0,1,3,0],
            [0,0,0,3]
           ]
 
+matriz2 :: [[Integer]]
 matriz2 = [[1,2,3,4,5],
            [2,2]
           ]
@@ -76,3 +78,50 @@ rabo = map tail
 
 cabeca :: Mat a -> [a]
 cabeca = map head 
+
+--e)
+-- Admitindo que as duas matrizes tenham a mesma ordem.
+
+multMat :: Num a => Mat a -> Mat a -> Mat a 
+multMat [] _ = []
+multMat (x:xs) m1 = linhaMat x m2 : multMat xs m1
+    where
+        (a:b) = m1
+        m2 = transpose m1
+
+linhaMat :: Num a => [a] -> Mat a -> [a]
+linhaMat _ [] = []
+linhaMat l m = map (linhaColuna l) m
+
+linhaColuna :: Num a => [a] -> [a] -> a
+linhaColuna (x:xs) (y:ys) = (x*y) + linhaColuna xs ys
+linhaColuna _ _ = 0
+
+--f)
+zipWMat :: (a -> b -> c) -> Mat a -> Mat b -> Mat c
+zipWMat f ([]:t1) (_:t2) = zipWMat f t1 t2
+zipWMat f ((x1:x2):xs) ((y1:y2):ys) = (f x1 y1 : a) : as
+    where
+        (a:as) = zipWMat f (x2:xs) (y2:ys)
+zipWMat _ _ _ = [[]]
+
+--g)
+
+triSup :: (Eq a, Num a) => Mat a -> Bool 
+triSup [] = True 
+triSup m = verdade (a:as) (n-1) && triSup (reverse t)
+    where
+        n = length m
+        ((a:as):t)  = reverse m
+
+verdade :: (Eq a, Num a ) => [a] -> Int -> Bool
+verdade [] _ = False
+verdade (x:xs) n | n > 0 && x == 0 = verdade xs (n-1)
+                 | n == 0    = True
+                 | otherwise = False
+
+--h)
+rotateLeft :: Mat a -> Mat a 
+rotateLeft [[]] = [[]]
+rotateLeft ([]:t) = []
+rotateLeft m = map last m : rotateLeft (map init m)
